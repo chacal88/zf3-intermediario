b5e952c1e1c566ce7dad7b85551a2e739e35baeb
Kaue Rodrigues <chacal@icaro>
2016-11-21 18:29:19 -0200

    log apache local doctrine

---
+++ config/autoload/doctrine.local.php.dist.php
@@ -0,0 +1,17 @@
+<?php
+
+
+return array(
+    'doctrine' => array(
+        'connection' => array(
+            // default connection name
+            'orm_default' => array(
+                'params' => array(
+                    'user' => '',
+                    'password' => '',
+                    'dbname' => '',
+                )
+            )
+        )
+    )
+);
